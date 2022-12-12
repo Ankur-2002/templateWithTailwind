e290b1cfd80c8a66016d91c8a9db8cd428d426ee
Ankur2002 <ankurchaurasia002@gmail.com>
2022-12-13 02:50:47 +0530

    final commit

--- src/compontents/Clients.jsx
+++ src/compontents/Clients.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import { clients } from '../constants';
+import styles from '../style';
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((item, index) => {
+          return (
+            <div
+              key={item.id}
+              className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+            >
+              <img
+                src={item.logo}
+                className="sm:w-[192px] w-[100px] object-contain"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
